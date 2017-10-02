d98915d400e1716190b181f2ace8a9c866b052da
Mark-43 <31340722+Zhuangwei-PENG@users.noreply.github.com>
2017-10-02 13:24:30 +0800

    Set icon and lanchimage

---
+++ PZWBlog/PZWBlog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PZWBlog
+//
+//  Created by Zhuangwei PENG on 2017/10/2.
+//  Copyright © 2017年 Zhuangwei PENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
